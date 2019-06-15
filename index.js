fd7fd418557b8b30de1b4fa4349d983c80209dc3
kly001 <karenly1961@gmail.com>
2019-06-15 12:07:23 -0400

    Initial Commit

--- index.js
+++ index.js
@@ -12,3 +12,11 @@ I need this code, just don't know where, perhaps should make some middleware, do
 
 Go code!
 */
+const express = require("express");
+const port = process.env.PORT ||3456;
+
+const server = express();
+
+server.listen(port, () => {
+    console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+  });
