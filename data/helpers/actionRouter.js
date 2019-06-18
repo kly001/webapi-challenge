7c00bdb3717c8883184d45715c7db0b5a2fcad56
kly001 <karenly1961@gmail.com>
2019-06-17 21:52:37 -0400

    ..

--- data/helpers/actionRouter.js
+++ data/helpers/actionRouter.js
@@ -16,6 +16,16 @@ router.get("/", async(req, res) => {
     }
   });
 
+  router.get("/:id", async(req, res) => {
+    try {
+      const actions = await Actions.get(req.params.id);
+      res.status(200).json(actions);
+    } catch (error) {
+      res.status(500).json({
+        message: 'Error getting the actions'});
+    }
+  });
+
   router.post("/", async(req,res)=> {
       try{
           const newAction = await Actions.insert(req.body);
